1f47446b7404ff30b7e8d37aad35bda5f5980de9
elcarim5efil <elcarim5efil@gmail.com>
2019-02-25 17:01:10 +0800

    test: add test for bmp

---
+++ test/bmp.test.js
@@ -0,0 +1,19 @@
+const getImageInfo = require('../src/index');
+const path = require('path');
+const ImageStream = require('../src/helpers/image-stream');
+const fs = require('fs');
+const stream = fs.createReadStream(path.resolve(__dirname, './assets/test.bmp'));
+
+test('jpg', () => {
+  return getImageInfo(stream)
+    .then((res) => {
+      const { stream, width, height, type } = res;
+
+      let buffer = [];
+
+      expect(type).toBe('bmp');
+      expect(width).toEqual(1170);
+      expect(height).toEqual(616);
+      expect(stream).toBeInstanceOf(ImageStream);
+  });
+});
