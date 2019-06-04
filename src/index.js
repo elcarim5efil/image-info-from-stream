9038ef948e38f0ddacc61a6374cf1151e6d649be
elcarim5efil <elcarim5efil@gmail.com>
2019-06-04 18:05:59 +0800

    feat: add callback mode

--- src/index.js
+++ src/index.js
@@ -14,57 +14,66 @@ const unknownTypeResult = {
   height: null 
 };
 
-function getImageInfo(stream) {
-  return new Promise((resolve, reject) => {
-    const readable = new Readable({
-      read() {}
-    });
-    let handler;
-    let result;
-    let variables = {};
-    let innerBuffer = [];
+function getImageInfoFromStream(stream, onSuccess) {
+  const readable = new Readable({
+    read() {}
+  });
+  let handler;
+  let result;
+  let variables = {};
+  let innerBuffer = [];
 
-    stream.on('data', (data) => {
-      // if the result is fetched, just add to the readable stream
-      if (result) {
-        readable.push(data);
-        return;
-      }
+  stream.on('data', (data) => {
+    // if the result is fetched, just add to the readable stream
+    if (result) {
+      readable.push(data);
+      return;
+    }
 
-      innerBuffer.push(data);
-      if (!handler) {
-        for(let i = 0; i < types.length; ++i) {
-          if (types[i].check(data)) {
-            handler = types[i];
-            break;
-          }
+    innerBuffer.push(data);
+    if (!handler) {
+      for(let i = 0; i < types.length; ++i) {
+        if (types[i].check(data)) {
+          handler = types[i];
+          break;
         }
       }
+    }
 
-      if (handler) {
-        result = handler.size(data, variables);
-      // cannot find handler
-      } else {
-        result = unknownTypeResult;
-      }
+    if (handler) {
+      result = handler.size(data, variables);
+    // cannot find handler
+    } else {
+      result = unknownTypeResult;
+    }
 
-      if (result) {
-        // if the result is fetched, flush the innerBuffer to a readable stream
-        innerBuffer.forEach(b => {
-          readable.push(b);
-        });
+    if (result) {
+      // if the result is fetched, flush the innerBuffer to a readable stream
+      innerBuffer.forEach(b => {
+        readable.push(b);
+      });
 
-        // resolve the result
-        resolve(Object.assign({}, result, {
-          stream: readable
-        }));
-      }
-    });
-
-    stream.on('end', () => {
-      readable.push(null);
-    })
+      // resolve the result
+      onSuccess(Object.assign({}, result, {
+        stream: readable
+      }));
+    }
   });
+
+  stream.on('end', () => {
+    readable.push(null);
+  })
+}
+
+function getImageInfo(stream, callback) {
+  const useCallback = typeof callback === 'function';
+  if (useCallback) {
+    getImageInfoFromStream(stream, callback);
+  } else {
+    return new Promise(resolve => {
+      getImageInfoFromStream(stream, resolve);
+    });
+  }
 }
 
 module.exports = getImageInfo;
